cb42c803789ce19e2becee4b79a3b75b3a734a4a
Antonio Ricardo <antonio.eschola@gmail.com>
2017-05-23 13:51:32 -0400

    Adding field image to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523172153) do
+ActiveRecord::Schema.define(version: 20170523174741) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170523172153) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
